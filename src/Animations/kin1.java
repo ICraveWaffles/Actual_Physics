d61af0469b05624c7514fdb0df0cdcbe619ad7f2
louis <louis.pisvin@gmail.com>
2026-02-08 19:08:54 +0100

    First try, first try!

--- src/Animations/kin1.java
+++ src/Animations/kin1.java
@@ -1,6 +1,6 @@
 package Animations;
-
 import Classes.Block;
+import Classes.Graph;
 import processing.core.PApplet;
 
 public class kin1 extends PApplet {
@@ -15,43 +15,75 @@ public class kin1 extends PApplet {
 
     Block b1;
 
-    public void setup(){
-        b1 = new Block (200, 900, 13, 0, 0, 0, 50, 50, 1);
-    }
+    Graph gPos;
+    Graph gVel;
+    Graph gAcc;
 
-    public void settings(){
+    public void settings() {
         fullScreen();
     }
 
-    public void draw(){
-        t = (float) ((millis() - startTime) / 1000.0);
+    public void setup() {
+        startTime = millis();
+
+        b1 = new Block(200, 900, 11, 0, 0, 0, 50, 50, 1);
+
+        gPos = new Graph(this, 50, 50, 400, 150, 300, 0, 1500);
+        gVel = new Graph(this, 530, 50, 400, 150, 300, -32, 32);
+        gAcc = new Graph(this, 1010, 50, 400, 150, 300, -0.2f, 0.2f);
+    }
+
+    public void draw() {
+        t = (millis() - startTime) / 1000.0f;
         beats = t * w;
 
         background(0);
-        b1.display(this);
-        b1.update();
 
-        if (beats > 2 && !beatOne){
+        b1.update();
+        b1.display(this);
+
+        if (beatOne && !beatTwo){
+            gVel.actualizar(2*b1.vy);
+        } else {
+            gVel.actualizar(2*sqrt(b1.vx * b1.vx + b1.vy * b1.vy));
+        }
+        if (!beatOne) {
+            gPos.actualizar(b1.x - 200);
+        } else if (!beatTwo){
+            gPos.actualizar(-b1.y + 900);
+        } else {
+            gPos.actualizar(dist(200, 900, b1.x, b1.y));
+        }
+        gAcc.actualizar(b1.ay);
+
+        gPos.display();
+        gVel.display();
+        gAcc.display();
+
+        if (beats > 2 && !beatOne) {
             b1.vx = 0;
-            b1.ay = -0.3f;
-            b1.vy = -20;
+            b1.ay = -0.16f;
+            b1.vy = -11;
             beatOne = true;
         }
-        if (beats > 4 && !beatTwo){
+
+        if (beats > 4 && !beatTwo) {
             b1.vx = -11;
-            b1.vy = -20;
-            b1.ay = -0.29f;
+            b1.vy = -11;
+            b1.ay = -0.16f;
             beatTwo = true;
         }
 
-        if (beats > 6){
+        if (beats > 6) {
             b1.ay = 0;
             b1.vx = 0;
             b1.vy = 0;
         }
     }
 
-    public static void main(String[]args){
+    public static void main(String[] args) {
         PApplet.main("Animations.kin1");
     }
 }
+
+
---
+++ src/Classes/Graph.java
@@ -0,0 +1,69 @@
+package Classes;
+
+import processing.core.PApplet;
+
+public class Graph {
+
+    public PApplet p;
+
+    public float[] datos;
+    public int maxDatos;
+    public int x, y, w, h;
+
+    public float minY;
+    public float maxY;
+
+    public Graph(PApplet p, int x, int y, int w, int h, int maxDatos, float minY, float maxY) {
+        this.p = p;
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.maxDatos = maxDatos;
+        this.minY = minY;
+        this.maxY = maxY;
+
+        datos = new float[maxDatos];
+    }
+
+    public void actualizar(float valor) {
+        for (int i = 0; i < maxDatos - 1; i++) {
+            datos[i] = datos[i + 1];
+        }
+        datos[maxDatos - 1] = valor;
+    }
+
+    public void display() {
+        p.pushStyle();
+
+        // marco
+        p.noFill();
+        p.stroke(255);
+        p.rect(x, y, w, h);
+
+        // ----- línea roja discontinua en y = 0 -----
+        if (minY < 0 && maxY > 0) {
+            float yZero = p.map(0, minY, maxY, y + h, y);
+
+            p.stroke(255, 0, 0);
+            float dash = 10;
+            float gap = 6;
+
+            for (float xx = x; xx < x + w; xx += dash + gap) {
+                p.line(xx, yZero, p.min(xx + dash, x + w), yZero);
+            }
+        }
+
+        p.stroke(0, 150, 255);
+        p.noFill();
+        p.beginShape();
+        for (int i = 0; i < datos.length; i++) {
+            float vx = p.map(i, 0, datos.length - 1, x, x + w);
+            float vy = p.map(datos[i], minY, maxY, y + h, y);
+            p.vertex(vx, vy);
+        }
+        p.endShape();
+
+        p.popStyle();
+    }
+}
